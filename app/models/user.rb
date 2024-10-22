f61cc55f456e1d87fc42d6d760d1a985df521398
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-10-22 21:38:07 +0100

    Basic User validations and User model spec began

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   has_person_name
   has_one_attached :avatar
 
+  validates :first_name, :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
