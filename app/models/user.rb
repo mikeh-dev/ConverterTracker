6e6ac0df9032420e1892bc211d7ca78f3c1ac258
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-10-22 21:52:33 +0100

    Added factory bot for users, tweaked the user model spec and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,14 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+  
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe "validations" do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
